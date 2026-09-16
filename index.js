62c78ad87b6309ab9558475048f003ef75e34d18
Alex McCleery <mccleeryalex@gmail.com>
2026-09-16 17:26:56 -0400

    getProfeciencyBonus() returns the character's bonus

--- index.js
+++ index.js
@@ -95,6 +95,22 @@ function heal(maxHp, currentHp, healAmount) {
  */
 function getProficiencyBonus(level, rank) {
   // TODO
+  const TRAINED_BONUS = 2;
+  const EXPERT_BONUS = 4;
+  const MASTER_BONUS = 6;
+  const LEGENDARY_BONUS = 8;
+
+  if (rank === 'trained') {
+    return level + TRAINED_BONUS;
+  } else if (rank === 'expert') {
+    return level + EXPERT_BONUS;
+  } else if (rank === 'master') {
+    return level + MASTER_BONUS;
+  } else if (rank === 'legendary') {
+    return level + LEGENDARY_BONUS;
+  } else {
+    return 0;
+  }
 }
 
 /**
